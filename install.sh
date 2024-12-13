6317caa02daae94d063ca4bf40a29ef20b2cd47d
Marius Johnsen <marius.johnsen@outlook.com>
2024-12-13 21:49:13 +0100

    refactor part 3

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 SHELLSMITH_DIR="${SHELLSMITH_ROOT:-$HOME/.config/.shellsmith}"
 
-source "$SCRIPT_DIR/utils/app_interface.sh"
-
 mkdir -p "$SHELLSMITH_DIR/apps"
 
-safe_symlink "$SCRIPT_DIR/ShellSmith.sh" "/usr/local/bin/smith"
-safe_symlink "$SCRIPT_DIR/utils" "$SHELLSMITH_DIR/utils"
+ln -s "$SCRIPT_DIR/utils" "$SHELLSMITH_DIR/utils"
+sudo ln -s "$SCRIPT_DIR/ShellSmith.sh" "/usr/local/bin/smith"
