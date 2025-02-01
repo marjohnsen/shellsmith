b094c890e3571845fa4192473f3808c32254faea
Marius Johnsen <marius.johnsen@outlook.com>
2025-02-01 23:04:06 +0100

    simplify app_handler logic

--- app.sh
+++ app.sh
@@ -6,24 +6,21 @@ set -E
 
 trap 'echo "[ERROR] Command \"${BASH_COMMAND}\" failed at line ${LINENO} in script ${BASH_SOURCE[0]}. Exiting..." >&2; exit 1' ERR
 
-SHELLSMITH_DIR="${SHELLSMITH_WORKSPACE:-$HOME/.config/shellsmith}"
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 APPS=()
 
-if [[ ! -d "$SHELLSMITH_DIR/apps" ]]; then
-  echo "Error: Apps directory '$SHELLSMITH_DIR/apps' is missing."
+if [[ ! -d "$SHELLSMITH_WORKSPACE/apps" ]]; then
+  echo "Error: Apps directory '$SHELLSMITH_WORKSPACE/apps' is missing."
   echo "Please create a valid workspace."
-  echo "See 'smith workspace help' for more information."
 
   exit 1
 fi
 
-cd "$SHELLSMITH_DIR/apps" || return
+cd "$SHELLSMITH_WORKSPACE/apps" || return
 
 export SHELLSMITH_DIR
-source "$SCRIPT_DIR/app/app_handler.sh"
-source "$SCRIPT_DIR/app/dependency_handler.sh"
-source "$SCRIPT_DIR/app/app_installer.sh"
+source "$SHELLSMITH_ROOT/app/app_handler.sh"
+source "$SHELLSMITH_ROOT/app/dependency_handler.sh"
+source "$SHELLSMITH_ROOT/app/app_installer.sh"
 
 app_handler APPS
 
